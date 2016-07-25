62b3fea8fd61e3063d376a0efda77110062f5d96
ptoapanta <pablo_vtn@hotmail.com>
2016-07-24 19:40:29 -0500

    Subimission Project Week 1 Exploratory Analysis

---
+++ plot1.R
@@ -0,0 +1,15 @@
+dataSet <- read.table("household_power_consumption.txt", header=T, sep=";")
+dataSet$Date <- as.Date(dataSet$Date, format="%d/%m/%Y")
+df <- dataSet[(dataSet$Date=="2007-02-01") | (dataSet$Date=="2007-02-02"),]
+df$Global_active_power <- as.numeric(as.character(df$Global_active_power))
+df$Global_reactive_power <- as.numeric(as.character(df$Global_reactive_power))
+df$Voltage <- as.numeric(as.character(df$Voltage))
+df <- transform(df, timestamp=as.POSIXct(paste(Date, Time)), "%d/%m/%Y %H:%M:%S")
+df$Sub_metering_1 <- as.numeric(as.character(df$Sub_metering_1))
+df$Sub_metering_2 <- as.numeric(as.character(df$Sub_metering_2))
+df$Sub_metering_3 <- as.numeric(as.character(df$Sub_metering_3))
+
+#First Plot
+hist(df$Global_active_power, main = paste("Global Active Power"), col="red", xlab="Global Active Power (kilowatts)")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
